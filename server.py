fccd0b008f331efd2bc6d8c18012adc85c8cf604
Carolyn Lam <caro_lyn_@hotmail.com>
2016-10-18 17:04:55 -0400

    add all the files

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, flash
 import pg
 import datetime
+# import time
 
 app = Flask('if_you_care')
 db = pg.DB(dbname='volunteer_db')
@@ -8,10 +9,26 @@ app.secret_key = 'give_a_little'
 
 @app.route('/')
 def home_page():
+    count = session.get('count', 0)
+    session['count'] = count + 1
+
     return render_template(
         'homepage.html',
         title='If You Care'
     )
+
+@app.route('/login_handler')
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    query = db.query('select * from volunteer where email = $1', email)
+    results_list = namedresult()
+    if len(results_list[0]) > 0:
+        if results_list[0].password == password:
+            session['email']
+    else:
+        pass
+    return redirect('/')
 
 @app.route('/org_signup')
 def render_org_signup():
@@ -39,7 +56,7 @@ def submit_new_user():
     return redirect('/')
 
 @app.route('/submit_new_org', methods=['POST'])
-def submit_new_user():
+def submit_new_org():
     name = request.form.get('name')
     description = request.form.get('description')
 
@@ -53,9 +70,34 @@ def submit_new_user():
 
 @app.route('/projects')
 def view_projects():
+    query = db.query('select organization.name as Organization, project.name as Project, project.project_description as Description, project.start_date as Time from project, organization where project.organization_id = organization.id')
+    results_list = query.namedresult()
+
     return render_template(
         'projects.html',
-        title='If You Care'
+        title='If You Care',
+        entry_list= results_list
+    )
+
+@app.route('/search', methods=['POST'])
+def search_bar():
+    date = request.form.get('date')
+    query = db.query('select organization.name as Organization, project.name as Project, project.project_description as Description, project.start_date as Time from project, organization where project.organization_id = organization.id and start_date = $1', date)
+    results_list = query.namedresult()
+
+    print "DATE: %r" % date
+    return render_template(
+        'projects.html',
+        entry_list = results_list
+    )
+
+
+@app.route('/register_project')
+def register():
+
+    return render_template(
+        'register_project.html',
+        title='My Projects'
     )
 
 if __name__ == '__main__':
